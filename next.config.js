77e111c4b1ff15a23747650c79f7c41581ce49c5
noir <zxj8911@hotmail.com>
2024-11-11 17:13:33 +0800

    add vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['您的图片域名'], // 如果使用外部图片，需要添加域名
+  },
+}
 
 module.exports = nextConfig
